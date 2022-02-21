dc857cc8f6e64f8f831c9716e2a11192a5fd1c46
Benjamin Rosche <benjamin.rosche@gmail.com>
2022-02-21 14:50:11 -0600

    Updates to Website

---
+++ R/cps-data.R
@@ -0,0 +1,18 @@
+#' CPS sample
+#'
+#' @docType data
+#'
+#' @usage data(cps_sample)
+#'
+#'
+#' @keywords datasets
+#'
+#' @references Sarah Flood, Miriam King, Renae Rodgers, Steven Ruggles, J. Robert Warren and Michael Westberry.
+#' Integrated Public Use Microdata Series, Current Population Survey: Version 9.0 [dataset]. Minneapolis, MN: IPUMS, 2021.
+#' (\href{https://doi.org/10.18128/D030.V9.0}{https://doi.org/10.18128/D030.V9.0})
+#'
+#' @source \href{https://cps.ipums.org/cps/}{CPS Website}
+#'
+#' @examples
+#' data(cps_sample)
+"cps_sample"
---
+++ create/create_cps_sample.R
@@ -0,0 +1,43 @@
+# To create a CPS sample with all young mothers but only 1000 nonmothers each year
+
+library(haven)
+library(dplyr)
+library(ineqx)
+
+setwd("C:/Users/benja/OneDrive - Cornell University/GitHub/earningsinequality")
+
+data <-
+  read_dta("./data/cps_sheela.dta") %>%
+  dplyr::filter(N==2, bystart<=0) %>%
+  dplyr::mutate(year=year-1+minyear)
+
+nonmothers <-
+  data %>%
+  dplyr::select(id, year, n, mother) %>%
+  dplyr::filter(mother==0, n==1) %>%
+  group_by(year) %>%
+  slice_sample(n=1000) %>%
+  ungroup() %>%
+  dplyr::mutate(sample=1) %>%
+  dplyr::select(id, sample)
+
+cps_sample <-
+  data %>%
+  left_join(nonmothers, by="id") %>%
+  dplyr::mutate(
+    sample=case_when(
+      is.na(sample) & mother==1 ~ 1,
+      is.na(sample) & mother == 0 ~ 0,
+      TRUE ~ sample)) %>%
+  dplyr::filter(sample==1) %>%
+  dplyr::mutate(
+    bym_age = byear*mother*age
+  )
+
+rm(data, nonmothers)
+
+setwd("C:/Users/benja/OneDrive - Cornell University/GitHub/ineqx")
+
+save(cps_sample, file="./data/cps_sample.RData")
+
+
